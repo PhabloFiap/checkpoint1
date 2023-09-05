ac76ff269b8f2a8872a45bca508b5757fe3e183d
PhabloFiap <rm550687@fiap.com.br>
2023-09-05 17:49:55 -0300

    Começando a passar valores pela URI

--- src/routes/Produto/index.jsx
+++ src/routes/Produto/index.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+import { ListaProdutos } from "../../components/ListaProdutos"
+
+
 export default function Produtos(){
+
     return(
+
         <main>
             <h1>Produtos</h1>
+            {ListaProdutos.map(prod =>(
+                <div key= {prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
 
     )
