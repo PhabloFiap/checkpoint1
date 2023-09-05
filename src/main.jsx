ce63795bde52a6998d82b575d5a62301ec33a5ac
PhabloFiap <rm550687@fiap.com.br>
2023-09-05 17:18:41 -0300

    criar Link para navegar

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home/index.jsx'
 import Produto from './routes/Produto/index.jsx'
-import Error from'.routes/Error/index.jsx'
+import Error from'./routes/Error/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 
 const router = createBrowserRouter([{
@@ -33,6 +33,6 @@ const router = createBrowserRouter([{
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router ={router}/>
-    
+
   </React.StrictMode>,
 )
